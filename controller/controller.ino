ea87fc2bfff5dee14c6d62a340cfa55090dc2a53
PotatoCider <ciderpotato@gmail.com>
2025-10-28 12:16:03 +0800

    use bluetooth controller

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,6 +1,7 @@
 #include <esp_now.h>
 #include <WiFi.h>
 #include <esp_err.h>
+#include <Bluepad32.h>
 
 #define RIGHT_BUTTON    1
 #define UP_BUTTON       2
@@ -10,7 +11,69 @@
 #define A_BUTTON        21
 #define B_BUTTON        47
 
-uint8_t receiver_address[] = {0xd0, 0xcf, 0x13, 0x1d, 0x7f, 0x64};  // replace with your MAC address found with the other code
+ControllerPtr btController;
+
+void onConnectedController(ControllerPtr ctl) {
+  Serial.printf("CALLBACK: Controller is connected idx: %d\n");
+  ControllerProperties properties = ctl->getProperties();
+  Serial.printf("Found controller model: %s, VID=0x%04x, PID=0x%04x\n", ctl->getModelName().c_str(), properties.vendor_id,
+                 properties.product_id);
+  if (!ctl->isGamepad()) {
+    return;
+  }
+  Serial.printf("Using controller idx: %d\n", ctl->index());
+  btController = ctl;
+}
+
+void onDisconnectedController(ControllerPtr ctl) {
+  if (!btController) return;
+  if (ctl->index() == btController->index()) btController = NULL;
+}
+
+
+void dumpGamepad(ControllerPtr ctl) {
+    Serial.printf(
+        "idx=%d, dpad: 0x%02x, buttons: 0x%04x, axis L: %4d, %4d, axis R: %4d, %4d, brake: %4d, throttle: %4d, "
+        "misc: 0x%02x, gyro x:%6d y:%6d z:%6d, accel x:%6d y:%6d z:%6d\n",
+        ctl->index(),        // Controller Index
+        ctl->dpad(),         // D-pad
+        ctl->buttons(),      // bitmask of pressed buttons
+        ctl->axisX(),        // (-511 - 512) left X Axis
+        ctl->axisY(),        // (-511 - 512) left Y axis
+        ctl->axisRX(),       // (-511 - 512) right X axis
+        ctl->axisRY(),       // (-511 - 512) right Y axis
+        ctl->brake(),        // (0 - 1023): brake button
+        ctl->throttle(),     // (0 - 1023): throttle (AKA gas) button
+        ctl->miscButtons(),  // bitmask of pressed "misc" buttons
+        ctl->gyroX(),        // Gyro X
+        ctl->gyroY(),        // Gyro Y
+        ctl->gyroZ(),        // Gyro Z
+        ctl->accelX(),       // Accelerometer X
+        ctl->accelY(),       // Accelerometer Y
+        ctl->accelZ()        // Accelerometer Z
+    );
+}
+
+//void processControllers() {
+//    for (auto myController : myControllers) {
+//        if (myController && myController->isConnected() && myController->hasData()) {
+//            if (myController->isGamepad()) {
+//                processGamepad(myController);
+//            } else if (myController->isMouse()) {
+//                processMouse(myController);
+//            } else if (myController->isKeyboard()) {
+//                processKeyboard(myController);
+//            } else if (myController->isBalanceBoard()) {
+//                processBalanceBoard(myController);
+//            } else {
+//                Serial.println("Unsupported controller");
+//            }
+//        }
+//    }
+//}
+
+// 80:b5:4e:e3:e1:2c
+uint8_t receiver_address[] = {0x80, 0xb5, 0x4e, 0xe3, 0xe1, 0x2c};  // replace with your MAC address found with the other code
 esp_now_peer_info_t receiver_peer;
 
 typedef struct {
@@ -28,16 +91,39 @@ void setup() {
   while (!Serial);
   Serial.println("Hello World");
 
-  pinMode(RIGHT_BUTTON, INPUT_PULLUP);    // this means the pin when
-  // pinMode(LEFT_BUTTON,  INPUT_PULLUP);    // not pressed will be
-  pinMode(UP_BUTTON,    INPUT_PULLUP);    // read as HIGH or 1
-  // pinMode(DOWN_BUTTON,  INPUT_PULLUP);
-  pinMode(A_BUTTON,     INPUT_PULLUP);    // to use properly other
-  pinMode(B_BUTTON,     INPUT_PULLUP);    // button side to gnd
+  
+  Serial.printf("Firmware: %s\n", BP32.firmwareVersion());
+  const uint8_t* addr = BP32.localBdAddress();
+  Serial.printf("BD Addr: %2X:%2X:%2X:%2X:%2X:%2X\n", addr[0], addr[1], addr[2], addr[3], addr[4], addr[5]);
 
-  pinMode(JOYSTICK_VRX, INPUT);
-  pinMode(JOYSTICK_VRY, INPUT);
-  pinMode(JOYSTICK_BUTTON, INPUT_PULLUP);
+  // Setup the Bluepad32 callbacks
+  BP32.setup(&onConnectedController, &onDisconnectedController);
+
+  // "forgetBluetoothKeys()" should be called when the user performs
+  // a "device factory reset", or similar.
+  // Calling "forgetBluetoothKeys" in setup() just as an example.
+  // Forgetting Bluetooth keys prevents "paired" gamepads to reconnect.
+  // But it might also fix some connection / re-connection issues.
+  BP32.forgetBluetoothKeys();
+
+  // Enables mouse / touchpad support for gamepads that support them.
+  // When enabled, controllers like DualSense and DualShock4 generate two connected devices:
+  // - First one: the gamepad
+  // - Second one, which is a "virtual device", is a mouse.
+  // By default, it is disabled.
+  BP32.enableVirtualDevice(false);
+
+  delay(100);
+//  pinMode(RIGHT_BUTTON, INPUT_PULLUP);    // this means the pin when
+//  // pinMode(LEFT_BUTTON,  INPUT_PULLUP);    // not pressed will be
+//  pinMode(UP_BUTTON,    INPUT_PULLUP);    // read as HIGH or 1
+//  // pinMode(DOWN_BUTTON,  INPUT_PULLUP);
+//  pinMode(A_BUTTON,     INPUT_PULLUP);    // to use properly other
+//  pinMode(B_BUTTON,     INPUT_PULLUP);    // button side to gnd
+//
+//  pinMode(JOYSTICK_VRX, INPUT);
+//  pinMode(JOYSTICK_VRY, INPUT);
+//  pinMode(JOYSTICK_BUTTON, INPUT_PULLUP);
 
   WiFi.mode(WIFI_MODE_STA);   // Arduino's wifi library, internally sets up the espressif wifi stack
   // code crashes without the WiFi.mode(WIFI_MODE_STA); line.
@@ -51,7 +137,7 @@ void setup() {
   Serial.println("creating esp_now peer");
   Serial.flush();
   memcpy(receiver_peer.peer_addr, receiver_address, 6);
-  receiver_peer.channel = 0;
+  receiver_peer.channel = 1;
   receiver_peer.encrypt = false;
   // receiver_peer.lmk;   // used for encryption
   receiver_peer.ifidx = WIFI_IF_STA;
@@ -61,6 +147,7 @@ void setup() {
   esp_now_add_peer(&receiver_peer);
 
   Serial.println("Finished initialising ESP-now");
+    
   Serial.flush();
 }
 
@@ -69,22 +156,25 @@ int abs(int i) {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  curData.vrx = analogRead(JOYSTICK_VRX);
-  curData.vry = analogRead(JOYSTICK_VRY);
-  curData.button = digitalRead(JOYSTICK_BUTTON);
-  
-  Serial.printf("x: %d, y: %d, button: %d\n",
-                curData.vrx,
-                curData.vry,
-                curData.button
-               );
+  bool dataUpdated = BP32.update();
+  if (dataUpdated && btController && btController->isConnected() && btController->hasData()) {
+    curData.vrx = btController->axisRX(); // analogRead(JOYSTICK_VRX);
+    curData.vry = -btController->axisY(); // y axis is flipped for left joystick
+    // dumpGamepad(btController);
+  }
 
+   delay(50);
   if (
-    abs(curData.vrx - prevData.vrx) > 20 ||
-    abs(curData.vry - prevData.vry) > 20 ||
+    abs(curData.vrx - prevData.vrx) > 5 ||
+    abs(curData.vry - prevData.vry) > 5 ||
     curData.button != prevData.button
   ) {
+    Serial.printf("x: %d, y: %d, button: %d\n",
+                  curData.vrx,
+                  curData.vry,
+                  curData.button
+                 );
+
     memcpy(&prevData, &curData, sizeof(curData));
     esp_err_t error = esp_now_send(receiver_address, (uint8_t*)&curData, sizeof(curData));    // works for every primitive datatype
     Serial.println(esp_err_to_name(error));
@@ -95,5 +185,5 @@ void loop() {
       Serial.printf("error occured, increasing delay time next time");
     }
   }
-  delay(50);   // decrease for a higher polling rate, recommended to stay above 30ms due to ESP_NOW limitation
+  delay(100);   // decrease for a higher polling rate, recommended to stay above 30ms due to ESP_NOW limitation
 }
--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,51 +1,62 @@
 #include <esp_now.h>
+//#include <esp_mac.h>
 #include <WiFi.h>
+#include <Bluepad32.h>
 
-#define RIGHT_ENABLE 9
-#define LEFT_ENABLE 4
-#define RIGHT_FORWARD 8 // right wheel forward
-#define RIGHT_BACKWARD 7 // right wheel reverse
-#define LEFT_FORWARD 6 // left wheel forward
-#define LEFT_BACKWARD 5 // left wheel reverse
+// FRONT / 
+#define RIGHT_FORWARD 9 // right wheel forward (IN_1)
+#define RIGHT_BACKWARD 10 // right wheel reverse (IN_2)
+#define LEFT_FORWARD 11 // left wheel forward (IN_3)
+#define LEFT_BACKWARD 12 // left wheel reverse (IN_4)
+
+//#define RIGHT_FORWARD 46 // right wheel forward (IN_1)
+//#define RIGHT_BACKWARD 8 // right wheel reverse (IN_2)
+//#define LEFT_FORWARD 6 // left wheel forward (IN_3)
+//#define LEFT_BACKWARD 5 // left wheel reverse (IN_4)
 
 typedef struct {
-  int vrx; // 0 - 4095
-  int vry; // 0 - 4095
-  bool button;
+  int vel_x; // 0 - 4095
+  int vel_y; // 0 - 4095
+  int buttons; // button bitmap
 } message_data;
 
 #define STEADY_TOL 50
-#define STEADY_X 1940
-#define STEADY_Y 1940
-#define MAX_X (4096 - STEADY_X)
-#define MAX_Y (4096 - STEADY_Y)
-#define MIN_X (- STEADY_X)
-#define MIN_Y (-STEADY_Y)
+#define STEADY_X 0
+#define STEADY_Y 0
+#define MAX_X (511)
+#define MAX_Y (511)
+#define MIN_X (-512)
+#define MIN_Y (-512)
 
 message_data curData;
 
+ControllerPtr btController;
+
+void onConnectedController(ControllerPtr ctl) {
+  Serial.printf("CALLBACK: Controller is connected idx: %d\n");
+  ControllerProperties properties = ctl->getProperties();
+  Serial.printf("Found controller model: %s, VID=0x%04x, PID=0x%04x\n", ctl->getModelName().c_str(), properties.vendor_id,
+                 properties.product_id);
+  if (!ctl->isGamepad()) {
+    return;
+  }
+  Serial.printf("Using controller idx: %d\n", ctl->index());
+  btController = ctl;
+}
+
+void onDisconnectedController(ControllerPtr ctl) {
+  if (!btController) return;
+  if (ctl->index() == btController->index()) btController = NULL;
+}
+
 void OnDataRecv(uint8_t * mac, message_data *data, uint8_t len) {
   memcpy(&curData, data, len);
-//  Serial.print("Length received:");
-//  Serial.println(len);
-//  Serial.printf("x: %d, y: %d, button: %d\n",
-//                curData.vrx,
-//                curData.vry,
-//                curData.button
-//               );
-//  Serial.println();
-}
-
-void disable_motors() {
-  digitalWrite(RIGHT_ENABLE, LOW);
-  digitalWrite(LEFT_ENABLE, LOW);
-}
-
-void enable_motors() {
-  digitalWrite(RIGHT_ENABLE, HIGH);
-  digitalWrite(LEFT_ENABLE, HIGH);
-}
-
+  Serial.printf("x: %d, y: %d, buttons: 0x%x\n",
+                curData.vel_x,
+                curData.vel_y,
+                curData.buttons
+               );
+} 
 
 void setup() {
   // put your setup code here, to run once:
@@ -53,81 +64,112 @@ void setup() {
   while(!Serial);
   Serial.println("Hello World");
 
-  // setup WiFi
-  WiFi.mode(WIFI_STA);
+  uint8_t mac_addr[6];
+  esp_err_t ret = esp_base_mac_addr_get(mac_addr);
 
-  // Init ESP-NOW
-  if (esp_now_init() != ESP_OK) {
-    Serial.println("Error initializing ESP-NOW");
-    return;
+  if(ret == ESP_OK){
+    Serial.println("Mac address: ");
+    Serial.printf("%02x:%02x:%02x:%02x:%02x:%02x\n", mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  }else{
+    Serial.println("Failed to read Mac address");
   }
 
-  // register receiving callback
-  esp_now_register_recv_cb(
-    esp_now_recv_cb_t(OnDataRecv)
-  );
+  BP32.setup(&onConnectedController, &onDisconnectedController);
+  BP32.forgetBluetoothKeys();
+  BP32.enableVirtualDevice(false);
+  
+  // setup WiFi
+//  WiFi.mode(WIFI_STA);
 
-  pinMode(RIGHT_ENABLE, OUTPUT);
-  pinMode(LEFT_ENABLE, OUTPUT);
+  // Init ESP-NOW
+//  if (esp_now_init() != ESP_OK) {
+//    Serial.println("Error initializing ESP-NOW");
+//    return;
+//  }
+
+  // register receiving callback
+//  esp_now_register_recv_cb(
+//    esp_now_recv_cb_t(OnDataRecv)
+//  );
+
   pinMode(RIGHT_FORWARD, OUTPUT);
   pinMode(RIGHT_BACKWARD, OUTPUT);
   pinMode(LEFT_FORWARD, OUTPUT);
   pinMode(LEFT_BACKWARD, OUTPUT);
-  disable_motors();
-  enable_motors();
-  curData.vrx = STEADY_X;
-  curData.vry = STEADY_Y;
+
+  analogWriteResolution(10);
+//  analogWriteFrequency(100);
+  
+//  curData.vrx = STEADY_X;
+//  curData.vry = STEADY_Y;
 }
 
 void loop() {
-  int x = curData.vrx - STEADY_X;
-  int y = curData.vry - STEADY_Y;
+  static int vel_x = 0;
+  static int vel_y = 0;
+  static double scale = 1;
+  static bool prevPressed = false;
+  
+  bool dataUpdated = BP32.update();
+  if (dataUpdated && btController && btController->isConnected() && btController->hasData()) {
+    vel_x = btController->axisRX();
+    vel_y = -btController->axisY(); // y axis is flipped for left joystick
+    
+    if (!prevPressed && btController->y()) {
+      prevPressed = true;
+      if (scale == 1.0) {
+        scale = 0.5;
+      } else if (scale == 0.5) {
+        scale = 0.25;
+      } else if (scale == 0.25) {
+        scale = 0.125;
+      } else if (scale == 0.125) {
+        scale = 1.0;
+      }
+    } else if (!curData.buttons) {
+      prevPressed = false;
+    }
+  }
+
+  int x = vel_x * 2;
+  int y = vel_y * 2;
   int left_spd = 0;
   int right_spd = 0;
+
   if (abs(y) > STEADY_TOL) {
-    // move only if joystick deviates 50 units
-    if (y > 0) {
-      int pwm_y = y * 256 / MAX_Y;
-      left_spd = right_spd = pwm_y;
-    } else {
-      int pwm_y = y * 256 / MIN_Y;
-      left_spd = right_spd = -pwm_y;
-    }
+    left_spd = right_spd = y;
   }
-
   if (abs(x) > STEADY_TOL) {
-    if (x > 0) {
-      int pwm_x = x * 256 / MAX_X;
-      right_spd -= pwm_x * 2; // TODO: scale this properly
-    } else {
-      int pwm_x = y * 256 / MIN_X;
-      left_spd += pwm_x * 2; // TODO: scale this properly
-    }
-  }
-  
-  Serial.printf("left: %d, right: %d, \n", left_spd, right_spd);
-  if (left_spd == 0) {
-    analogWrite(LEFT_FORWARD, 0);
-    analogWrite(LEFT_BACKWARD, 0);
-  } else if (left_spd > 0) {
-    analogWrite(LEFT_FORWARD, left_spd);
-    analogWrite(LEFT_BACKWARD, 0);
-  } else {
-    analogWrite(LEFT_FORWARD, 0);
-    analogWrite(LEFT_BACKWARD, -left_spd);
+    right_spd -= x;
+    left_spd += x;
   }
 
-  if (right_spd == 0) {
+  int scaled_left_spd = left_spd * scale; // max(min(left_spd, 1024), -1024) * scale;
+  int scaled_right_spd = right_spd  * scale; // max(min(left_spd, 1024), -1024) * scale;
+  
+  Serial.printf("l: %d (%d), r: %d (%d), s: %f \n", scaled_left_spd, left_spd, scaled_right_spd, right_spd, scale);
+  if (scaled_left_spd == 0) {
+    analogWrite(LEFT_FORWARD, 0);
+    analogWrite(LEFT_BACKWARD, 0);
+  } else if (scaled_left_spd > 0) {
+    analogWrite(LEFT_FORWARD, scaled_left_spd);
+    analogWrite(LEFT_BACKWARD, 0);
+  } else {
+    analogWrite(LEFT_FORWARD, 0);
+    analogWrite(LEFT_BACKWARD, -scaled_left_spd);
+  }
+
+  if (scaled_right_spd == 0) {
     analogWrite(RIGHT_FORWARD, 0);
     analogWrite(RIGHT_BACKWARD, 0);
-  } else if (right_spd > 0) {
-    analogWrite(RIGHT_FORWARD, right_spd);
+  } else if (scaled_right_spd > 0) {
+    analogWrite(RIGHT_FORWARD, scaled_right_spd);
     analogWrite(RIGHT_BACKWARD, 0);
   } else {
     analogWrite(RIGHT_FORWARD, 0);
-    analogWrite(RIGHT_BACKWARD, -right_spd);
+    analogWrite(RIGHT_BACKWARD, -scaled_right_spd);
   }
 
   // put your main code here, to run repeatedly:
-  delay(100);
+  delay(50);
 }
